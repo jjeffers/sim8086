d94a971326e093ffae488b383c3e3b6a29638b6d
James Jeffers <jdjeffers@protonmail.com>
2025-02-21 09:45:09 -0500

    Moved instruction table build to a constructor.

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,16 @@ const OpCodeType = enum {
 const Instruction = struct {
     opcode: OpCodeType,
     size: u8,
+    asm_text: []const u8,
 };
+
+pub fn buildInstructionLookup(alloc: std.mem.Allocator) !std.AutoHashMap(u8, Instruction) {
+    var instructions_lookup = std.AutoHashMap(u8, Instruction).init(alloc);
+
+    try instructions_lookup.put(0b10001001, Instruction{ .opcode = OpCodeType.mov, .size = 2, .asm_text = "mov" });
+
+    return instructions_lookup;
+}
 
 pub fn main() !void {
     var allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -29,15 +38,16 @@ pub fn main() !void {
 
     try stdout.print("bits 16\n", .{});
 
-    var instructions_lookup = std.AutoHashMap(u8, Instruction).init(alloc);
-    defer instructions_lookup.deinit();
+    const instructions_lookup = try buildInstructionLookup(alloc);
 
-    try instructions_lookup.put(0b10001001, Instruction{ .opcode = OpCodeType.mov, .size = 2 });
-
-    try Disassemble(buffer, file_size, instructions_lookup, stderr);
+    try Disassemble(buffer, file_size, instructions_lookup, stdout, stderr);
 }
 
-pub fn Disassemble(memory_buffer: []u8, memory_size: u64, instructions_lookup: std.AutoHashMap(u8, Instruction), stderr: anytype) !void {
+pub fn printInstruction(inst: Instruction, stdout: anytype) !void {
+    try stdout.print("{s} \n", .{inst.asm_text});
+}
+
+pub fn Disassemble(memory_buffer: []u8, memory_size: u64, instructions_lookup: std.AutoHashMap(u8, Instruction), stdout: anytype, stderr: anytype) !void {
     var memory_buffer_index: u32 = 0;
 
     while (memory_buffer_index < memory_size) {
@@ -51,6 +61,8 @@ pub fn Disassemble(memory_buffer: []u8, memory_size: u64, instructions_lookup: s
                 try stderr.print("ERROR: Instruction extends outside disassembly region\n", .{});
                 break;
             }
+
+            try printInstruction(inst, stdout);
         } else {
             try stderr.print("ERROR: Unrecognized binary in instruction stream.\n", .{});
             break;
