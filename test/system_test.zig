4187de886079f95bc1c6eb0c4bd6e68ccb1e8242
James Jeffers <jdjeffers@protonmail.com>
2025-03-05 15:40:03 -0500

    Wrap up decode steps before final byte wise comparison of the listing and decoded asm files.

--- test/system_test.zig
+++ test/system_test.zig
@@ -46,22 +46,37 @@ fn compileAndCompareAssembly(listing: fs.Dir.Walker.Entry) !bool {
         const term = try nasm.wait();
         try std.testing.expectEqual(term.Exited, 0);
 
-        const sim8086_disassembly = try std.fmt.allocPrint(allocator, "{s}.asm", .{baseName});
+        const sim8086_disassembly = try std.fmt.allocPrint(allocator, "{s}.sim8086_asm", .{baseName});
         defer allocator.free(sim8086_disassembly);
+
+        const decode_file = try std.fs.cwd().createFile(sim8086_disassembly, .{});
+        defer decode_file.close();
 
         const sim8086_argv = [_][]const u8{
             "../zig-out/bin/sim8086",
             baseName,
-            ">",
-            sim8086_disassembly,
         };
 
         var sim8086 = Child.init(&sim8086_argv, allocator);
+        sim8086.stdout_behavior = .Pipe;
 
         try sim8086.spawn();
+        const stdout = sim8086.stdout.?.reader();
+        const stdout_writer = decode_file.writer();
+
+        const max_output_size = 100 * 1024 * 1024;
+        const bytes = try stdout.readAllAlloc(allocator, max_output_size);
+        defer allocator.free(bytes);
+
+        try stdout_writer.writeAll(bytes);
+
         const sim8086_term = try sim8086.wait();
 
         try std.testing.expectEqual(sim8086_term.Exited, 0);
+
+        const decode_stat = try std.fs.cwd().statFile(sim8086_disassembly);
+
+        try std.testing.expectEqual(decode_stat.kind, .file);
 
         const sim8086_assembly = try std.fmt.allocPrint(allocator, "{s}.sim8086", .{baseName});
         defer allocator.free(sim8086_assembly);
